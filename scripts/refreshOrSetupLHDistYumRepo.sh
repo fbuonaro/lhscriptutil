cf7f27ab181786079500f9db760ed6df35149365
Frank Buonarota <fbuonaro@gmail.com>
2022-09-12 19:33:05 -0400

    fix repo setup script

--- scripts/refreshOrSetupLHDistYumRepo.sh
+++ scripts/refreshOrSetupLHDistYumRepo.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 DIST_DIR="/lhdist"
-REPO_DIR="/lhdistrepo"
+REPO_NAME="lhdistrepo"
+REPO_DIR="/${REPO_NAME}"
 
 yum install -y createrepo yum-utils
 
 mkdir -p "${REPO_DIR}"
 
-cp -rf "${DIST_DIR}/*.rpm" "${REPO_DIR}"
+cp -rf -u "${DIST_DIR}"/*.rpm "${REPO_DIR}"
 
 createrepo "${REPO_DIR}"
 
-cat <<EOF > /etc/yum.repos.d/lhdistrepo.repo
-[lhdistrepo]
+cat <<EOF > /etc/yum.repos.d/${REPO_NAME}.repo
+[${REPO_NAME}]
 name=LHDist Repository
 baseurl=file://${REPO_DIR}
 gpgcheck=0
@@ -20,4 +21,3 @@ enabled=1
 EOF
 
 yum -y clean all
-yum -y update
